71e9ac71e9f34da0b3ea3c8363d07b93f61f6eea
Feodor Cherashev <mrcherry.pro@gmail.com>
2012-02-27 18:27:42 +0400

    Deleted some directories.

--- build/runbuildscript.bat
+++
@@ -1,6 +0,0 @@
-# This is for windows users only.
-# If you're on a mac or linux, just run `ant build` from this folder in Terminal
-
-set ANT_OPTS=-D"file.encoding=UTF-8"
-set MYDIR=%~dp0
-ant build
